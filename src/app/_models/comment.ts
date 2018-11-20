d78a14e24cfc12d7b2ffb2b127aa6dd007e29612
Miha Stele <stele1000@gmail.com>
2018-11-20 10:57:25 +0100

    cs

---
+++ src/app/_models/comment.ts
@@ -0,0 +1,7 @@
+// TODO use appropriate parameters...
+export class Comment { // album
+  userId: number;
+  userName: string;
+  commentMsg: string;
+  imgRef: string;
+}
--- src/app/view-image/view-image.component.ts
+++ src/app/view-image/view-image.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
+import {CommentService} from '../_services/comment.service';
+import {elementEnd} from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-view-image',
@@ -12,8 +14,11 @@ export class ViewImageComponent implements OnInit {
 
   public likenum = 0;
   public commentMsg = 'cyka';
+  public specificComments: Comment[];
 
-  constructor(route: ActivatedRoute) {
+  constructor(route: ActivatedRoute, private commentService: CommentService) {
+
+    /*
     const id: Observable<string> = route.params.pipe(map(p => p.id));
     const url: Observable<string> = route.url.pipe(map(segments => segments.join('')));
     // route.data includes both `data` and `resolve`
@@ -21,8 +26,10 @@ export class ViewImageComponent implements OnInit {
 
     const mock = route.queryParams.pipe(map( q => q.a));
 
+    mock.subscribe(data => console.log(data));*/
 
-    mock.subscribe(data => console.log(data));
+    this.commentService.getCommentForPost().subscribe(data => this.specificComments = data);
+
   }
 
   ngOnInit() {
