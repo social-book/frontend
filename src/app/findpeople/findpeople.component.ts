ee1ee2dc3a27974734ef55edd7b699abac6cb676
Miha Stele <stele1000@gmail.com>
2018-11-23 22:37:35 +0100

    ...

--- src/app/findpeople/findpeople.component.ts
+++ src/app/findpeople/findpeople.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {User} from '../_models';
 import {UserService} from '../_services';
 import {SharedDataService} from '../shared-data.service';
 import {first} from 'rxjs/operators';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-findpeople',
@@ -12,11 +13,24 @@ import {first} from 'rxjs/operators';
 })
 export class FindpeopleComponent implements OnInit {
 
-  users$: Observable<User[]>;
+  // users$: Observable<User[]>;
+  addedUsers: User[];
+  avilableToAdd: User[];
   availableUsers: Array<User>;
 
   constructor(protected userService: UserService, public sd: SharedDataService) {
+
+    this.userService.getFriends(sd.getLoggedInUser().userId).subscribe(data => this.addedUsers = data);
+
+    /*
+    this.userService.getAll().subscribe(data => data.forEach(function (item) {
+
+    }));
+
+    /*
     this.users$ = userService.getAll();
+
+
     sd.user = new User(); // todo get it from logged in id
     this.users$.pipe(first()).subscribe(data  => {
     sd.user.username = data[0].username;
@@ -25,6 +39,8 @@ export class FindpeopleComponent implements OnInit {
     sd.user.imgref = data[0].imgref;
     sd.user.name = data[0].name;
     sd.user.surname = data[0].surname;
+
+
   });
 
     this.availableUsers = new Array<User>();
@@ -37,7 +53,7 @@ export class FindpeopleComponent implements OnInit {
         }
 
       }
-    );
+    );*/
 
   }
 
