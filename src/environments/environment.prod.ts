f65cb7d14915960b10e3f5b2eda3ca7f0e0c19e8
mihastele <stele1000@gmail.com>
2018-12-11 20:08:22 +0100

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,16 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://159.122.186.89:31340/v1',
+  apiUrl: 'http://159.122.186.89:31340/v1', // user url
+  apiAlbumUrl: 'http://159.122.186.89:31777/v1',
+  apiImageUrl: 'http://159.122.186.89:31175',
+  apiCommentUrl: 'http://159.122.186.89:31311/v1',
+  mockUrl: 'http://localhost:3000',
+  uploads: '/v1/uploads',
   user_path: 'users',
   album_path: 'albums',
-  comments_path: 'comments',
+  comment_path: 'comments',
+  like_path: 'likes',
+  category_path: 'categories',
   notfications_path: 'notfications'
 
 };
